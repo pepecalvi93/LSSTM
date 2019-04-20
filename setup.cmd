378e0adb19f30a6a93c78c52cab06beb0b2d5ea1
Giuseppe G <ggc115@ic.ac.uk>
2019-04-20 18:44:31 +0100

    added setup file for environment

---
+++ setup.cmd
@@ -0,0 +1,4 @@
+conda update conda
+conda create -n lsstm python=3.6
+conda activate lsstm
+pip install -r requirements.txt
--- test_snp.py
+++ test_snp.py
@@ -8,12 +8,13 @@ raw_data = pd.read_csv('./finance_data/raw_data.csv')
 data_svm = pd.read_csv('./finance_data/raw_data_svm.csv')
 data_stm = load_obj('data_stm')
 
+C = 10
+sig2 = 10
 strat = Strategy(raw_data = raw_data)
-results = strat.stm(data_stm=data_stm, tensor_size=[2,3,2], lookBack=250, C=10, kernel='RBF', sig2=1, verbose=True)
-
+results = strat.stm(data_stm=data_stm, tensor_size=[3,3,2], lookBack=250, C=C, kernel='RBF', sig2=sig2, verbose=True)
 results_stm = results['Performance']
 results_stm.plot(figsize=(15,5))
-
+plt.title('Cost = {}, sigma = sig2={}'.format(C, sig2))
 plt.show()
 
-print(results_stm['Accuracy'])
+print(results['Accuracy'])
