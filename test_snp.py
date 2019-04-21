d3cde0b32113316ff68e43fc0d9c11acabbee03c
Giuseppe G <ggc115@ic.ac.uk>
2019-04-21 20:31:27 +0100

    minor changes

--- test_snp.py
+++ test_snp.py
@@ -8,10 +8,10 @@ raw_data = pd.read_csv('./finance_data/raw_data.csv')
 data_svm = pd.read_csv('./finance_data/raw_data_svm.csv')
 data_stm = load_obj('data_stm')
 
-C = 50
+C = 10
 sig2 = 100
 strat = Strategy(raw_data = raw_data)
-results = strat.stm(data_stm=data_stm, tensor_size=[2,3,2], lookBack=250, lag=True, C=C, kernel='RBF', sig2=sig2, verbose=True)
+results = strat.stm(data_stm=data_stm, tensor_size=[2,3,2], lookBack=250, lag=True, C=C, kernel='linear', sig2=sig2, verbose=True)
 results_stm = results['Performance']
 results_stm.plot(figsize=(15,5))
 plt.title('Cost = {}, sigma = sig2={}'.format(C, sig2))
